1a1fc2b45e482bb20268e144ad062b1d3a453493
Wascharapong Phummon <rickyharvey.w@gmail.com>
2024-06-06 15:04:09 +0700

    Feat: Message Board

--- src/components/MessageBoard.jsx
+++ src/components/MessageBoard.jsx
@@ -1,23 +1,61 @@
+import { useState } from "react";
+
 function MessageBoard() {
+  const [message, setMessage] = useState([]);
+  const [messageText, setMessageText] = useState("");
+
+  const handleMessageTextChange = (event) => {
+    setMessageText(event.target.value);
+  };
+
+  const addMessage = (event) => {
+    event.preventDefault();
+    if (messageText.trim()) {
+      const newMessage = [...message];
+      newMessage.push(messageText);
+      setMessage(newMessage);
+      setMessageText("");
+    }
+  };
+
+  const deleteMessage = (messageIndex) => {
+    const newMessage = [...message];
+    newMessage.splice(messageIndex, 1);
+    setMessage(newMessage);
+  };
+
   return (
     <div className="app-wrapper">
-      <h1 class="app-title">Message board</h1>
-      <div class="message-input-container">
+      <h1 className="app-title">Message board</h1>
+      <div className="message-input-container">
         <label>
           <input
             id="message-text"
             name="message-text"
             type="text"
             placeholder="Enter message here"
+            onChange={handleMessageTextChange}
+            value={messageText}
           />
         </label>
-        <button className="submit-message-button">Submit</button>
+        <button className="submit-message-button" onClick={addMessage}>
+          Submit
+        </button>
       </div>
-      <div class="board">
-        <div className="message">
-          <h1>Hello all ! This is first message.</h1>
-          <button className="delete-button">x</button>
-        </div>
+      <div className="board">
+        {message.map((item, index) => (
+          <div className="message" key={index}>
+            <h1>{item}</h1>
+            <button
+              className="delete-button"
+              onClick={() => {
+                deleteMessage(index);
+              }}
+            >
+              x
+            </button>
+          </div>
+        ))}
       </div>
     </div>
   );
